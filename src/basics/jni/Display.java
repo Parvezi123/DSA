2c292968f407eb9d41c6421dc8086ee7e66d49c8
Parvez <parvezimohammed@gmail.com>
2025-02-04 07:09:19 +0530

    Working of System.out.println()

--- src/basics/jni/Display.java
+++ src/basics/jni/Display.java
@@ -2,6 +2,8 @@ package basics.jni;
 
 public class Display {
     public static void main(String[] args) {
+        //During Runtime println() method is called and overloaded
         System.out.println();
+        System.out.println("Hello World!");
     }
 }
